6f16129fb6b4e18366cc2f4fb5ea2c556df2b5e2
Luciano Édipo <luciano.psilva@anpd.gov.br>
2025-03-26 08:55:25 -0300

    test: bump automatico2

--- scripts/bump-version.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs")
-const pkg = require("../package.json")
-
-const [major, minor, patch] = pkg.version.split(".").map(Number)
-const newVersion = `${major}.${minor}.${patch + 1}`
-pkg.version = newVersion
-
-fs.writeFileSync("./package.json", JSON.stringify(pkg, null, 2))
-console.log(`Version bumped to ${newVersion}`)
